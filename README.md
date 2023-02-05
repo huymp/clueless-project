6bcd816cbe8e2b6e59a798c5dc5eb582ec1f381c
Huy Pham <hikaruhuy83@gmail.com>
2023-02-04 22:47:44 -0800

    simple gRPC client/server application

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # clueless-project
+create virtual environment
+install gRPC in the virtual environment
+
+set appropriate Python path
---
+++ source/server_impl.py
@@ -0,0 +1,31 @@
+import grpc
+from concurrent import futures
+import time
+import proto.clueless_pb2_grpc as pb2_grpc
+import proto.clueless_pb2 as pb2
+
+
+class CluelessService(pb2_grpc.CluelessServicer):
+  def __init__(self, *args, **kwargs):
+    pass
+
+  def GetServerResponse(self, request, context):
+    # get the string from the incoming request
+    message = request.message
+    result = f'Hello I am up and running received "{message}" message from you'
+    result = {'message': result, 'received': True}
+
+    return pb2.MessageResponse(**result)
+
+
+def serve():
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+  pb2_grpc.add_CluelessServicer_to_server(CluelessService(), server)
+  server.add_insecure_port('[::]:50051')
+  server.start()
+  print("gRPC server started, listening on [::]:50051")
+  server.wait_for_termination()
+
+
+if __name__ == '__main__':
+  serve()
